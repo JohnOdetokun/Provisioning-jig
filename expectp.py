94ca7409c711168877bcf615be96ba5563977f6e
John Odetokun <rbcs.john@gmail.com>
2015-07-01 13:58:51 +0000

    abstracted code

--- expectp.py
+++ expectp.py
@@ -1,55 +1,42 @@
 #!/usr/bin/env python
 
-import pexpect
-import RPi.GPIO as GPIO
-import re
-import os
-import sys
+
+
+
 import time
-import telnetlib
 import getpass
 import signal
 import select
 #from my_file import MyClass
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(7,GPIO.OUT)
-GPIO.output(7,0)
+
+
 OCDstatus = True
 while True:
-    GPIO.wait_for_edge(11, GPIO.RISING)
-    GPIO.output(7, GPIO.input(11))
+    from button import Button
+    bu = Button()
+
+    while bu.butpush():
+        print("Button pushed")
+        bu.clean()
+    
+    from lights import Lights
+    c = Lights()
+    
     if(OCDstatus):
         from open_ocd import OpenOCD
         a = OpenOCD()
         OCDstatus = False
 
-
-    tel = pexpect.spawn("telnet localhost 4444", timeout = 3)
-    tel.expect("[\s\S]*\nConnected to localhost[\s\S]*")
-    print("connected to telnet")
-
-    tel.sendline("reset halt")
-    tel.expect("[\s\S]*target halted[\s\S]*")
-    print("halted")
-
-    tel.sendline("flash erase_sector 0 0 0")
-    tel.expect("[\s\S]*erased sectors[\s\S]*")
-    print("erased")
-
-    tel.sendline("flash write_image erase /home/pi/Downloads/demo.elf")
-    tel.expect("[\s\S]*wrote[\s\S]*from file[\s\S]*")
-
-    tel.sendline("verify_image /home/pi/Downloads/demo.elf ")
-    tel.expect("[\s\S]*verified[\s\S]*bytes in[\s\S]*")
-
-    print("LOADED!")
-
-    #close telnet
-    tel.send("exit")
-    User = raw_input("Press<enter> to exit or <spacebar> then <enter> to continue")
+        from tel_con import TelCon
+        b = TelCon()
+    b.halt()
+    b.erase()
+    b.load()
+    c.success()
+    
+    
+    User = input("Press<enter> to exit or <spacebar> then <enter> to continue")
     if (User == ""):
         break
-        
 
-GPIO.cleanup()
+c.clean()
--- open_ocd.py
+++ open_ocd.py
@@ -1,46 +1,34 @@
 import pexpect
 import time
-import RPi.GPIO as gpio
 import os
 import sys
 
 class OpenOCD:
     
     def __init__(self):
-        gpio.setmode(gpio.BOARD)
-        #fail LED
-        gpio.setup(12,gpio.OUT)
-        gpio.output(12,0)
-        
+        from lights import Lights
+        c = Lights()
         self.ocd = pexpect.spawn("openocd -f interface/stlink-v2.cfg -f target/stm32f0x_stlink.cfg", timeout=3)
         test1 = self.ocd.expect(["Open On-Chip Debugger[\s\S]*hardware has 4 breakpoints, 2 watchpoints[\s\S]*","[\s\S]*Error: open failed[\s\S]*","[\s\S]*Error: couldn't bind to socket: Address already in use[\s\S]*"])
         if test1 == 0:
             print("On-Chip debugger opened")
         elif test1 == 1:
             print("openOCD failed")
-            gpio.output(12,1)
-            time.sleep(1)
-            gpio.output(12,0)
-            time.sleep(1)
-            gpio.output(12,1)
-            time.sleep(2)
-            gpio.cleanup()
+            c.failed()
             sys.exit()
-        else:
+        elif test == 2:
             os.system("sudo killall openocd")
             print("openocd already active.... Now closed, run program again")
-            gpio.output(12,1)
-            time.sleep(1)
-            gpio.output(12,0)
-            time.sleep(1)
-            gpio.output(12,1)
-            time.sleep(2)
-            gpio.cleanup()
+            c.failed()
+            sys.exit()
+
+        else:
+            c.failed()
             sys.exit()
 
     def __exit__(self):
         #close openocd
-        time.sleep(5)
+        time.sleep(3)
         self.ocd.sendcontrol('c')
         self.ocd.kill()
 
